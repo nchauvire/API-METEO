56b916e54b568aa45a245da10e235bb5abab0f95
n.chauvire <nchauvire@gmail.com>
2017-10-02 14:03:25 +0200

    .

--- app/controllers/meteo.js
+++ app/controllers/meteo.js
@@ -1,8 +1,15 @@
 var express  = require('express');
 var router   = express.Router();
+var http = require('http');
+var meteoService = require('../services/meteoService');
+var pollutionService = require('../services/pollutionService');
+
+
 
 //affiche la map avec tout les hotels
 router.get('/', function(req, res, next) {
+	var polServ = new pollutionService('test');
+  console.log(polServ.fooBar());
   res.render('index', {title: 'Info sur le logement'});
 });
 
