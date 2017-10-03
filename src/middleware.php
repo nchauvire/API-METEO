91e74c052a1ac70f8660d95f312e7060da1c0ef4
n.chauvire <nchauvire@gmail.com>
2017-10-03 11:17:37 +0200

    add cors

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,6 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+use Src\Middlewares\CorsMW;
+
+$app->add(CorsMW::class);
--- src/routes.php
+++ src/routes.php
@@ -9,19 +9,20 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Appel de la route racine ");
 
-
     // Render index view
-    return $response->withJson("Merci de voir la documentation de l'api méteo",200);
+    return $response->withJson("Merci de voir la documentation de l api meteo",200);
 });
 
-$app->get('/prevision[/:city]', function (Request $request, Response $response, array $args) {
+$app->get('/prevision[/{city}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Appel de la route prévision pour la ville ".$args['city']);
 
+    if(!$args['city']){
+       //throw new Exception("Vous devez spécifier une ville");
+        return $response->withJson("Vous devez spécifier une ville",403);
+    }
     $client = new GuzzleHttp\Client();
-    $res = $client->request('GET', 'http://www.prevision-meteo.ch/services/json/lat=47.218371ng=-1.553621');
+    $res = $client->request('GET', 'api.openweathermap.org/data/2.5/weather?q='.$args['city'].'&appid=e6221bb27e53441f7f7582f9be12ea9a');
 
-    var_dump($res);
-    // Render index view
-    return $response->withJson("",200);
+    return $response->write($res->getBody()->getContents(),200);
 });
